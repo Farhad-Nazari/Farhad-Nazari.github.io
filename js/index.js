c06367632a2d328d0bd07431251f2fea3193b10a
Farhad-Nazari <farhadnazari.nioc@gmail.com>
2022-09-19 16:47:42 +0430

    portfolio page
    created and loaded

--- js/index.js
+++ js/index.js
@@ -1,211 +1,223 @@
-/* cookies */
-const cookies = document.querySelector(".cookies-container");
-const cookiesClose = document.querySelector(".cookies-icon");
-
-window.addEventListener("load",function(){
-    cookies.classList.remove ("hidden");
-});
-cookiesClose.addEventListener("click",function(){
-    cookies.classList.add("hidden");
-});
-window.addEventListener("click",function(x){
-    if (x.target == cookies) {
-        cookies.classList.add("hidden");
-    }
-});
-
-/* link to top */
-const fixedElement = document.querySelector(".fixed-element");
-
-document.addEventListener("scroll",function(){
-    let sizeOfScroll = this.documentElement.scrollTop;
-    if (sizeOfScroll>100) {
-        fixedElement.classList.remove("hide-fixed-element");
-    }
-    else {
-        fixedElement.classList.add("hide-fixed-element");
-    }
-});
-
-/* search */
-const search = document.querySelector(".search-i");
-const searchForm = document.querySelector(".search-form");
-const formInput = document.querySelector(".inputNav");
-const formI= document.querySelector(".form-i");
-const form = document.querySelector(".formNav");
-
-search.addEventListener("click",function() {
-    searchForm.classList.toggle("showForm");
-    searchForm.classList.toggle("hideForm");
-    search.classList.toggle("bi-search");
-    search.classList.toggle("bi-x-lg");
-});
-
-window.addEventListener ("click",function(x) {
-    if (x.target !== searchForm && x.target!==search && x.target !== formInput && x.target!==form && x.target !==formI) {
-    searchForm.classList.remove("showForm");
-    searchForm.classList.add("hideForm");
-    search.classList.remove("bi-x-lg");
-    search.classList.add("bi-search");
-    }
-});
-
-/* navigation */
-const bars = document.querySelectorAll(".bars");
-const menuClose = document.querySelector(".menuClose");
-const submenuLi = document.querySelectorAll(".submenu-li");
-const submenu = document.querySelectorAll(".submenu");
-const back = document.querySelectorAll(".back");
-const navIcon = document.querySelector(".navIcon");
+/* Navigation */
+const icon = document.querySelector(".icon");
 const nav = document.querySelector(".navigation");
+const mobileMenu = document.querySelector(".mobile-menu");
+const li = document.querySelectorAll(".navigation-li");
+const lang = document.querySelector(".lang");
+const fa = document.querySelector(".fa");
+const en = document.querySelector(".EN");
+const faMobile = document.querySelector(".fa-mobile");
 
-
-navIcon.addEventListener("click", function() {
-    nav.classList.toggle ("showNav");
-    bars[0].classList.toggle("rotateRight");
-    bars[1].classList.toggle("hidden-bar");
-    bars[2].classList.toggle("rotateLeft");
-    menuClose.classList.toggle("menu-close");
-    if (menuClose.textContent=="MENU") {
-        menuClose.textContent="CLOSE"
-    }
-    else {
-        menuClose.textContent="MENU"
-    }
+icon.addEventListener("click",function() {
+    nav.classList.toggle("shownav");
+    icon.classList.toggle("bi-list");
+    icon.classList.toggle("icon-2");
+    icon.classList.toggle("bi-x");
+    en.classList.toggle("hide");
+    mobileMenu.classList.toggle("mobile-2");
 });
 
-for(let i=0; i<3 ; i++) {
-    submenuLi[i].addEventListener("click",function(){
-        submenu[i].classList.remove("hideNav");
-        submenu[i].classList.add("showNav");
-    });
-    
-    back[i].addEventListener("click",function(){
-        submenu[i].classList.remove ("showNav");
-        submenu[i].classList.add ("hideNav");
+for (let i=0;i<=5;i++) {
+    li[i].addEventListener("click",function(){
+        nav.classList.remove("shownav");
+        icon.classList.add("bi-list");
+        icon.classList.remove("icon-2");
+        icon.classList.remove("bi-x");
+        mobileMenu.classList.remove("mobile-2");
     });
 }
 
-/*slider1*/
-
-const previous = document.querySelector(".slider1-previous");
-const next = document.querySelector(".slider1-next");
-const slider = document.querySelector(".slider1-container");
-const row3Img = document.querySelector(".slider1-img");
-let counter = 0;
-let translateSlider1;
-
-function slider1() {
-  slider.style.transform = `translateX(${-translateSlider1*counter}px)`; 
-}
-
-next.addEventListener("click",function(){
-  let screen = window.innerWidth;
-  translateSlider1=row3Img.width;
-  counter++;
-  if (screen<=575 && counter<10) {
-    slider1();
-  }
-  else if (screen<=575 && counter==10) {
-    counter=0; 
-    slider1();
-  }
-  else if (screen>575 && counter<9) {
-    slider1();
-  }
-  else if (screen>575 && counter==9) {
-    counter=0; 
-    slider1();
-  }
+lang.addEventListener("click",function(){
+    fa.classList.toggle("hide");
+});
+en.addEventListener("click",function(){
+    faMobile.classList.toggle("hide");
 });
 
-previous.addEventListener("click",function(){
-  let screen = window.innerWidth;
-  translateSlider1=row3Img.width;
-  counter--;
-  if (screen<=575 && counter==-1) {
-    counter=9;
-    slider1();
-  }
-  else if(screen<=575 && counter<9) {
-    slider1();
-  }
-  else if (screen>575 && counter==-1) {
-    counter=8;
-    slider1(); 
-  }
-  else if (screen>575 && counter<8) {
-    slider1();
-  }
-});
+/* End of  Navigation */
 
-/*slider2*/
-const next2 = document.querySelector(".slider2-next");
-const previous2 = document.querySelector(".slider2-previous");
-const slide2 = document.querySelector(".slider2-container");
-const row6Img = document.querySelector(".row6-img");
-let counter2 = 0;
+/* Slider */
+
+const slide = document.querySelector(".slider");
+const image = document.querySelector(".slider-img");
+const control = document.querySelectorAll(".slider-i");
+let counter=0;
+let i=0;
 let widthOfTranslate;
 
-function slideActive(x) {
-  x.classList.remove ("notActive");
-  x.classList.add ("active");
-  slide2.style.transform = `translateX(-${widthOfTranslate*counter2}px)`; 
+for (let i=0;i<=8;i++) {
+    control[i].addEventListener("click",function() {
+        widthOfTranslate = image.width;
+        counter=i;
+        slide.style.transform = `translateX(-${counter*widthOfTranslate}px)`;
+        for(let j=0;j<=8;j++) {
+            control[j].classList.remove("bi-circle-fill");
+            control[j].classList.add("bi-circle");
+        }
+        control[i].classList.remove("bi-circle");
+        control[i].classList.add("bi-circle-fill");
+    })
 }
-function slideNonActive(x) {
-  x.classList.remove ("active");
-  x.classList.add ("notActive");
-  slide2.style.transform = `translateX(-${widthOfTranslate*counter2}px)`; 
+
+function next() {
+    counter++;
+        widthOfTranslate = image.width;
+        slide.style.transform = `translateX(-${counter*widthOfTranslate}px)`;
+        control[counter-1].classList.add("bi-circle");
+        control[counter-1].classList.remove("bi-circle-fill");
+        control[counter].classList.remove("bi-circle");
+        control[counter].classList.add("bi-circle-fill");
 }
-function slider2Next(x) {
-  widthOfTranslate= row6Img.width;
-  counter2++;
-  if (counter2<x && counter2>0) {
-    slideActive(previous2);
-  }
-  else if (counter2==x) {
-    slideNonActive(next2);
-  }
-  else {
-    counter2=x;
-  }
+
+function previous() {
+    counter--;
+        widthOfTranslate = image.width;
+        slide.style.transform = `translateX(-${counter*widthOfTranslate}px)`;
+        control[counter+1].classList.add("bi-circle");
+        control[counter+1].classList.remove("bi-circle-fill");
+        control[counter].classList.remove("bi-circle");
+        control[counter].classList.add("bi-circle-fill");
 }
-function slider2Previous(x) {
-  widthOfTranslate= row6Img.width;
-  counter2--;
-  if(counter2<x && counter2>0) {
-    slideActive(next2);
-  }
-  else if (counter2==0) {
-    slideNonActive(previous2);
-  }
-  else {
-  counter2=0;
-  }
-}
-next2.addEventListener("click",function(){
-  let screen = window.innerWidth;
-  if (screen<=767){
-    slider2Next(5);
-  }
-  else if (screen>=768 && screen<=991) {
-    slider2Next(4);
-  }
-  else if (screen>=992) {
-    slider2Next(3);
-  }
-});
-previous2.addEventListener("click",function(){
-  let screen = window.innerWidth;
-  if (screen<=767){
-    slider2Previous(5);
-  }
-  else if (screen>=768 && screen<=991) {
-    slider2Previous(4);
-  }
-  else if (screen>=992) {
-    slider2Previous(3);
-  }
+
+setInterval(function(){
+    let screen = window.innerWidth;
+    if (screen>991 && i==0 && counter<6) {
+       next();
+    }
+    else if (screen>991 && i==0 && counter==6) {
+       previous();
+        i=1;
+    }
+    else if (screen>991 && i==1 && counter>0) {
+       previous();
+    }
+    else if (screen>991 && i==1 && counter==0) {
+        next();
+        i=0;
+    }
+    else if (screen<=991 && screen>575 && i==0 && counter<7) {
+        next();
+     }
+     else if (screen<=991 && screen>575 && i==0 && counter==7) {
+        previous();
+         i=1;
+     }
+     else if (screen<=991 && screen>575 && i==1 && counter>0) {
+        previous();
+     }
+     else if (screen<=991 && screen>575 && i==1 && counter==0) {
+         next();
+         i=0;
+     }
+    else if (screen<=575 && i==0 && counter<8) {
+        next();
+     }
+     else if (screen<=575 && i==0 && counter==8) {
+        previous();
+         i=1;
+     }
+     else if (screen<=575 && i==1 && counter>0) {
+        previous();
+     }
+     else if (screen<=575 && i==1 && counter==0) {
+         next();
+         i=0;
+     }
+     else {
+        counter=0;
+        i=0;
+     }
+},5000);
+
+/* end of slider */
+
+const skills = document.querySelector("#skills");
+const html = document.querySelector(".html");
+const css = document.querySelector(".css");
+const bootstrap = document.querySelector(".bootstrap");
+const less = document.querySelector(".less");
+const js = document.querySelector(".javascript");
+const jQ = document.querySelector(".jQuery");
+const service = document.querySelector("#services");
+const services = document.querySelectorAll(".services-icon");
+let count=0;
+
+document.addEventListener(("scroll"),function(){
+    let percent=0;
+    let x= skills.offsetTop;
+    let sizeOfScroll = this.documentElement.scrollTop;
+    if (sizeOfScroll>(x-200) && count==0){
+        count=1;
+        setInterval(function(){
+            percent++;
+            if (percent>90) {
+                clearInterval;
+            }
+            else {
+                html.style.width=`${percent}%`;
+                css.style.width=`${percent}%`;
+            }
+        },50);
+
+        setInterval(function(){
+            percent++;
+            if (percent>85) {
+                clearInterval;
+            }
+            else {
+                bootstrap.style.width=`${percent}%`;
+            }
+        },50);
+
+        setInterval(function(){
+            percent++;
+            if (percent>85) {
+                clearInterval;
+            }
+            else {
+                less.style.width=`${percent}%`;
+            }
+        },50);
+
+        setInterval(function(){
+            percent++;
+            if (percent>75) {
+                clearInterval;
+            }
+            else {
+                js.style.width=`${percent}%`;
+                jQ.style.width=`${percent}%`;
+            }
+        },60)
+    }
+    
+    let y=service.offsetTop;
+    if (sizeOfScroll>y-100) {
+        for (let i=0;i<4;i++) {
+            services[i].style.transform="translateX(0)";
+        }
+    }
 });
 
-/*End*/
+/* --------------------------------------------------------------------------------*/
+
+const emailDiv=document.querySelector(".email-div-container");
+const emailBtn=document.querySelector(".email");
+const emailClose = document.querySelector(".email-close");
+const contactMe = document.querySelector(".contact-me");
+
+emailBtn.addEventListener("click",function(){
+    emailDiv.classList.add("show-email");
+});
+contactMe.addEventListener("click",function(){
+    emailDiv.classList.add("show-email");
+});
+emailClose.addEventListener("click",function(){
+    emailDiv.classList.remove("show-email");
+});
+window.addEventListener("click",function(x){
+    if (x.target == emailDiv) {
+        emailDiv.classList.remove("show-email");
+    }
+});
+
